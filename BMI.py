eddbdeb23fbb8319b6d43a1f89359f90fd1144be
Surya2294 <47246824+Surya2294@users.noreply.github.com>
2019-02-27 19:15:25 +0530

    Update BMI.py

--- BMI.py
+++ BMI.py
@@ -1,25 +1,27 @@
-#BMI = (weight in kg / height in meters squared )
+#!/usr/bin/env python3.6
 
 def gather_info():
-    height = float(input("what is your height ? (inches or meters"))
-    weight = float(input("what is your height ? (ponnds or imperial"))
-    system = input("Are your mesurements in metric or imperial units ?").lower().strip()
+    height = float(input("What is your height? (inches or meters) "))
+    weight = float(input("What is your weight? (pounds or kilograms) "))
+    system = input("Are your mearsurements in metric or imperial systems? ").lower().strip()
     return (height, weight, system)
-def calculate_bmi(weight, height, system='metri'):
+
+def calculate_bmi(weight, height, system='metric'):
     if system == 'metric':
         bmi = (weight / (height ** 2))
-    else
-        bmi = 703 * (weight / (height ** 2) )
+    else:
+        bmi = 703 * (weight / (height ** 2))
     return bmi
-while true:
-    height, weight , system = gather_info()
-    if system.startswitch('i'):
-        bmi = calculate_bmi(weight, system='imperial',height=height)
-        print(f"your BMI is {bmi}")
+
+while True:
+    height, weight, system = gather_info()
+    if system.startswith('i'):
+        bmi = calculate_bmi(weight, system='imperial', height=height)
+        print(f"Your BMI is {bmi}")
         break
-    elif system.stattswitch('m')
+    elif system.startswith('m'):
         bmi = calculate_bmi(weight, height)
-        print(f"tour BMI is {bmi}")
+        print(f"Your BMI is {bmi}")
         break
     else:
-        print("Error: unknown measurements system . pleas provide metric or imperial units")
+        print("Error: Unknown measurement system. Please use imperial or metric.")
